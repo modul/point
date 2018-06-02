8c853ec9b7446095e5b64b2d0fe10e1339666242
Remo Giermann <mo@liberejo.de>
2018-06-02 18:18:54 +0200

    statistics can be toggled, too

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 
 data Ball = Ball {
-             run :: Bool,            -- ^ run movement
+             run :: Bool,             -- ^ run movement
+             showStats :: Bool,       -- ^ show statistics 
              velocity :: Float,       -- ^ current velocity
              height :: Float,         -- ^ initial height
              gravity :: Float,        -- ^ gravity 
@@ -26,17 +27,18 @@ moveBall dt b@Ball{..} = trace (show (v', s')) $ go
                  if v < 0.1 then 0 else bounce * v
                 else velocity - gravity * dt
 
-initialState b h g r = Ball True (-g) h g h r b
+initialState b h g r = Ball True True (-g) h g h r b
 
 drawBall Ball{..} = Color red $ Translate 0 position $ circleSolid radius
 
 drawText size = Scale size size . Text
 
-drawStats Ball{..} = Translate 0 (-0.5 * height - radius) $ pictures [box, vel, pos, bnc]
+drawStats Ball{..} = Translate 0 (-0.5 * height - radius) $ pictures (box : stats)
     where box = rectangleWire 450 height
           pos = Translate (-190)    10  $ drawText size $ "s: " ++ (show position)
           vel = Translate    10     10  $ drawText size $ "v: " ++ (show velocity)
           bnc = Translate (-190)  (-25) $ drawText size $ "b: " ++ (show bounce)
+          stats = if showStats then [pos, vel, bnc] else []
           size = 0.20
           height = 80
 
@@ -55,11 +57,12 @@ background = white
 
 update = moveBall
 
-handle (EventKey (SpecialKey KeySpace   ) Down _ _) ball@Ball{..} = ball {run = (not run)}
+handle (EventKey (SpecialKey KeySpace   ) Down _ _) ball@Ball{..} = ball {run = not run}
 handle (EventKey (SpecialKey KeyDown    ) Down _ _) ball@Ball{..} = ball {velocity = (-2) * gravity }
 handle (EventKey (SpecialKey KeyEnter   ) Down _ _) ball@Ball{..} = ball {position = height, velocity = (-gravity)}
 handle (EventKey (SpecialKey KeyPageUp  ) Down _ _) ball@Ball{..} = ball {bounce = bounce + 0.05}
 handle (EventKey (SpecialKey KeyPageDown) Down _ _) ball@Ball{..} = ball {bounce = if bounce > 0.5 then bounce - 0.05 else bounce}
+handle (EventKey (SpecialKey KeyF1)       Down _ _) ball@Ball{..} = ball {showStats = not showStats}
 handle _ b = b
 
 infiniteBounce = initialState 1.0 300
